fbf3c1b7b69965fca62fe939c5ba91314c0587f9
Muhammad Nursalli <shallimuhammad@gmail.com>
2022-06-21 19:49:48 +0700

    Creating a Game Concept

--- app.js
+++ app.js
@@ -110,5 +110,16 @@ app.listen(port, () => {
 // User_game_histories:
 // 1. id (INT NOT NULL AUTO_INCREMENT)
 // 2. id_user (INT NOT NULL FOREIGN KEY)
-// 3. time (INT NOT NULL)
-// 4. score (INT NOT NULL)
+// 3. time (INT NOT NULL) Auto + 10/game
+// 4. room_id (VARCHAR NOT NULL UNIQUE)
+// 5. Pil1 (ENUM('R', 'S', 'P', 'Empty') DEFAULT 'Empty'
+// 6. Pil1 (ENUM('R', 'S', 'P', 'Empty') DEFAULT 'Empty'
+// 7. Pil1 (ENUM('R', 'S', 'P', 'Empty') DEFAULT 'Empty'
+// 8. status_room (win/Lose/Empty)
+// 9. score (INT NOT NULL)
+
+// Alur Game:
+// 1. User bikin room (input:name) -> pertama kali? -> tambah data history: id_user, time(0), room_id(name+5 angka random), status_room(empty), score(0) -> bukan -> update data history: room_id(name+5 angka random), status_room(empty)
+// 2. Join game (/join)(input:name) -> cek apakah nama room ada + cek apakah nama room sdh di pake 2 user -> pertama kali? -> tambah data history: id_user, time(0), room_id(name -> copy dari nama yg sdh ada), score(0)
+// 3. Game (play/room_id) -> cek apakah nama room ada + cek apakah room_id sdh 2 org yg pake + cek apakah player 1 sudah input pilihan? kalau belum -> player 2 input pilihan1, pilihan2, pilihan3 -> menunggu player 1 | kalau sdh -> player 2 input pilihan1, pilihan2, pilihan3 sekaligus kalkulasi hasil permainan (logic) + update score(menang: 10, kalah 0) + update time(player 1 dan 2 + 10) dan update status_room
+// 4. cek hasil permain (play/room_id/hasil) -> menampilkan room_id, pilihan1, pilihan2, pilihan3, dan status_room
